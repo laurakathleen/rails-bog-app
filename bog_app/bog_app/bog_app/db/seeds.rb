56b087e0afcf694baa5629a89a255e5387e4e195
laurakathleen <laurakwalley@gmail.com>
2016-10-01 16:56:34 -0700

    sets up file structure and gets index route

---
+++ bog_app/bog_app/bog_app/db/seeds.rb
@@ -0,0 +1,11 @@
+Creature.create({ name: "Yoda", description: "A little green guy"})
+Creature.create({ name: "Luke", description: "A cool guy"})
+
+
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
