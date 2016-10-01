11e471dd61465316bf13f398d4d5ab44e6acc752
laurakathleen <laurakwalley@gmail.com>
2016-10-01 14:57:19 -0700

    gets update working

--- bog_app/bog_app/app/controllers/creatures_controller.rb
+++ bog_app/bog_app/app/controllers/creatures_controller.rb
@@ -20,4 +20,22 @@ class CreaturesController < ApplicationController
   	creature_id = params[:id]
   	@creature = Creature.find_by_id(creature_id)
   end
+
+  def edit
+  	creature_id = params[:id]
+  	@creature = Creature.find_by_id(creature_id)
+  end
+
+  def update
+  	creature_id = params[:id]
+  	@creature = Creature.find_by_id(creature_id)
+  	@creature.update_attributes(creature_params)
+  	redirect_to creature_path(@creature)
+  end
+
+  private
+
+  def creature_params
+  	params.require(:creature).permit(:name, :description)
+  end
 end
