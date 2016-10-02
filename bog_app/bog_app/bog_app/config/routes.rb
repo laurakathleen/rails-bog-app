aa54ee5aaf3baa8ba646d855fa1ddf0623cfff61
laurakathleen <laurakwalley@gmail.com>
2016-10-01 17:06:33 -0700

    gets create route

--- bog_app/bog_app/bog_app/config/routes.rb
+++ bog_app/bog_app/bog_app/config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
 get "/", to: "creatures#index"
 get "/creatures", to: "creatures#index", as: "creatures"
+get "/creatures/new", to: "creatures#new", as: "creature_new"
+post "/creatures", to: "creatures#create"
 end
