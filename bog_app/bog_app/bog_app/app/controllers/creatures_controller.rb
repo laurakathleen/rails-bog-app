ecda5717d3546f82e58a4c09b032cd242142449e
laurakathleen <laurakwalley@gmail.com>
2016-10-01 17:25:32 -0700

    gets edit and update working

--- bog_app/bog_app/bog_app/app/controllers/creatures_controller.rb
+++ bog_app/bog_app/bog_app/app/controllers/creatures_controller.rb
@@ -9,7 +9,6 @@ def new
 end
 
 def create
-  creature_params = params.require(:creature).permit(:name, :description)
   creature = Creature.new(creature_params)
 
   if creature.save
@@ -20,6 +19,24 @@ end
 def show
   creature_id = params[:id]
   @creature = Creature.find_by_id(creature_id)
+end
+
+def edit
+  creature_id = params[:id]
+  @creature = Creature.find_by_id(creature_id)
+end
+
+def update
+  creature_id = params[:id]
+  @creature = Creature.find_by_id(creature_id)
+  @creature.update_attributes(creature_params)
+  redirect_to creature_path(creature_id)
+end
+
+private
+
+  def creature_params
+    params.require(:creature).permit(:name, :description)
 end
 
 end
--- bog_app/bog_app/bog_app/config/routes.rb
+++ bog_app/bog_app/bog_app/config/routes.rb
@@ -4,5 +4,6 @@ get "/creatures", to: "creatures#index", as: "creatures"
 get "/creatures/new", to: "creatures#new", as: "creature_new"
 post "/creatures", to: "creatures#create"
 get "/creatures/:id", to: "creatures#show", as: "creature"
-# edit "creatures/:id", to: "creatures#edit", as: "creature_edit"
+get "creatures/:id/edit", to: "creatures#edit", as: "creature_edit"
+patch "creatures/:id", to: "creatures#update"
 end
